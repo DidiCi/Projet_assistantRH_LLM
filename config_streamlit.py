0070c3aa15fece539be3d02ac5240f29f0591af7
Jip Wulffelé <jipwulffele@gmail.com>
2025-09-12 12:16:11 +0200

    change base modl mistral small

--- config_streamlit.py
+++ config_streamlit.py
@@ -9,11 +9,11 @@ class AppConfig:
     chunk_size: int = 800
     chunk_overlap: int = 100
     image_resolution_scale: float = 2.0
-    split_by_header: bool = True
+    split_by_header: bool = False
     one_chunk_cv: bool = False
 
     model_embeddings: str = "mistral-embed"
-    llm_model: str = "mistral-tiny"
+    llm_model: str = "mistral-small"
     llm_temperature: float = 0.3
 
     k: int = 10
